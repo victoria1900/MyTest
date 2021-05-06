0af79ef5443cb853e1d59a90ea01ac11209d9849
Victoria1900 <victoriadmitrievna@bk.ru>
2021-05-06 17:16:03 +0500

    week4| A game

---
+++ task/js/main.js
@@ -0,0 +1,17 @@
+// var slides = document.querySelectorAll('.slides .slide');
+// const slide1 = document.querySelector('.slider-list__item--slide1');
+// var currentSlide = 0;
+// var slideInterval = setInterval(nextSlide,3000);
+
+// function nextSlide() {
+//     slides[currentSlide].className = 'slide';
+//     currentSlide = (currentSlide+1)%slides.length;
+//     slides[currentSlide].className = 'slide showing';
+// };
+//  var swiper = new Swiper('.swiper-container', {
+    
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+//   });
